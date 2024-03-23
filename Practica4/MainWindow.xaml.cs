3b7c146dac456850363ad6e17b6677644318d4a9
staakumi <s.ilin2036@gmail.com>
2024-03-23 15:42:14 +0300

    Добавьте файлы проекта.

---
+++ Practica4/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Practica4.Hobby_shopDataSetTableAdapters;
+
+namespace Practica4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Shop_EmployeesTableAdapter employees = new Shop_EmployeesTableAdapter();
+        ProductsTableAdapter products = new ProductsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            EmployeesDataGrid.ItemsSource = employees.GetData();
+
+
+            ProductsDataGrid.ItemsSource = products.GetData();
+            EmployeesComboBox.ItemsSource = employees.GetData();
+            EmployeesComboBox.DisplayMemberPath = "Employees_Last_Name";
+        }
+        private void EmployeesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(EmployeesComboBox.SelectedItem != null)
+            {
+                var ID = (int)(EmployeesComboBox.SelectedItem as DataRowView).Row[0];
+                EmployeesDataGrid.ItemsSource = employees.FilterByName(ID);
+            }
+        }
+        private void ClearButtonClick(object sender, RoutedEventArgs e)
+        {
+            EmployeesDataGrid.ItemsSource = employees.GetData();
+        }
+        public void ShowEmployees_Click(object sender, RoutedEventArgs e)
+        {
+
+           
+
+            EmployeesDataGrid.Visibility = Visibility.Visible;
+            ProductsDataGrid.Visibility = Visibility.Collapsed;
+        }
+        public void ShowProducts_Click(object sender, RoutedEventArgs e)
+        {
+
+           
+
+            EmployeesDataGrid.Visibility = Visibility.Collapsed;
+            ProductsDataGrid.Visibility = Visibility.Visible;
+        }
+        private void OpenNewWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window = new Window2();
+            window.Show();
+        }
+        private void SearchButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (EmployeesDataGrid.Visibility == Visibility.Visible)
+            {
+            EmployeesDataGrid.ItemsSource = employees.SearchByName(SearchTxt.Text);
+
+            }
+            else
+            {
+                ProductsDataGrid.ItemsSource = products.SearchByNamePro(SearchTxt.Text);
+            }
+        }
+    }
+}
---
+++ Practica4/Window2.xaml.cs
@@ -0,0 +1,63 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica4
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private Hobby_shopEntities context = new Hobby_shopEntities();
+        public Window2()
+        {
+            InitializeComponent();
+            EmployeesDgr.ItemsSource = context.Shop_Employees.ToList();
+
+
+
+            ProductsDgr.ItemsSource = context.Products.ToList();
+
+        }
+        private void ShowEmployees_ClickEF(object sender, RoutedEventArgs e)
+        {
+
+       
+
+            EmployeesDgr.Visibility = Visibility.Visible;
+            ProductsDgr.Visibility = Visibility.Collapsed;
+        }
+        private void ShowProducts_ClickEF(object sender, RoutedEventArgs e)
+        {
+
+           
+
+            EmployeesDgr.Visibility = Visibility.Collapsed;
+            ProductsDgr.Visibility = Visibility.Visible;
+        }
+
+        private void SearchButtonClick(object sender, RoutedEventArgs e)
+        {
+            if(EmployeesDgr.Visibility == Visibility.Visible)
+            {
+
+            EmployeesDgr.ItemsSource = context.Shop_Employees.ToList().Where(item => item.Employees_Last_Name.Contains(SearchTxt.Text));
+            }
+            else
+            {
+                ProductsDgr.ItemsSource = context.Products.ToList().Where(item => item.Products_Name.Contains(SearchTxt.Text));
+            }
+        }
+    }
+}
